3268218cbc30e9f93a0b56bbc066ddb6776314ef
Ronald Emanuel <emronald15el@gmail.com>
2021-08-13 16:43:17 +0000

    feat: web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/series', [SeriesController::class, 'index']);
+Route::get('/series/create', [SeriesController::class, 'create']);
+Route::post('/series', [SeriesController::class, 'store']);
+Route::delete('series/{id}', [SeriesController::class, 'destroy']);
