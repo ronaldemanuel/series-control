8ada23a835b2fd52d6fb8ad754e26fc1310b21ce
Ronald Emanuel <emronald15el@gmail.com>
2021-08-13 18:33:48 +0000

    fix: margin button

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -16,7 +16,7 @@
             </div>
         </div>
     @endif
-    <a href="/series/create" class="btn btn-dark">Adicionar</a>
+    <a href="/series/create" class="btn btn-dark mb-2">Adicionar</a>
     <ul class="list-group">
         @foreach ($series as $series)
             <li class="list-group-item d-flex justify-content-between align-items-center">
