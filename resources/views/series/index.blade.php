468dcc9db7593b75c3dd2d5f35b26b7bd432edb4
Ronald Emanuel <emronald15el@gmail.com>
2021-08-13 18:08:34 +0000

    feat: add series button

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -12,10 +12,11 @@
         <div class="alert alert-success d-flex align-items-center" role="alert">
             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
             <div>
-            {{ session('msg') }}
+                {{ session('msg') }}
             </div>
         </div>
     @endif
+    <a href="/series/create" class="btn btn-dark">Adicionar</a>
     <ul class="list-group">
         @foreach ($series as $series)
             <li class="list-group-item">
